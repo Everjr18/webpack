b0ab9a7d2b0bd212f18cda14d3316b8e4e2772b9
Ever Rojas <Everjosejr@hotmail.com>
2022-04-28 20:10:22 -0500

    ✏️ - fix typos in webpack.config.js and srripts

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
-const CssMinimizerPlugin = require("css-minimizer-webpack-Plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 const Dotenv = require("dotenv-webpack");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
@@ -89,6 +89,6 @@ optimization:{
         new TerserPlugin(),
 
     ]
-
+ 
 }
 }
